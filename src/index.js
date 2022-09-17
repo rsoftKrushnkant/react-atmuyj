b87f3e831aa0e5a17fd15065d0932544cc67e428
Rsoft Krushnkant Team <109036897+rsoftKrushnkant@users.noreply.github.com>
2022-09-17 10:49:14 -0400

    Upload Theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React, { StrictMode } from 'react';
+// Internet Explorer 11 requires polyfills and partially supported by this project.
+// import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/stable';
+import './i18n';
+import './styles/app-base.css';
+import './styles/app-components.css';
+import './styles/app-utilities.css';
 import { createRoot } from 'react-dom/client';
+import App from './app/App';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
-import App from './App';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+reportWebVitals();
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
